be16faefc5f994c12924e7ff481d2f126f4b533e
Eduardo Alvarez <opticoedu@gmail.com>
2024-02-01 14:16:47 -0300

    hasta aqui llegue

--- Node_Apuntes_Ruan/AVALIAÇÃO_FINAL_NODE/ORM_Sequelize_Cervejaria/controllers/produtos.js
+++ Node_Apuntes_Ruan/AVALIAÇÃO_FINAL_NODE/ORM_Sequelize_Cervejaria/controllers/produtos.js
@@ -11,7 +11,32 @@ const cadastrarCerveja = async (req, res) => {
         const resultado = await Cerveja.create(novaCerveja)
         res.status(201).send({message: 'Cerveja cadastrada com sucesso!!', data: resultado})
     } catch (err) {
-        res.satus(500).send({ message: 'Houve um erro na criação' })
+        res.status(500).send({ message: 'Houve um erro na criação' })
+    }
+}
+
+const listarCervejas = async (_, res) => {
+    try {
+        const listaCervejas = await Cerveja.findAll()
+        res.status(200).send({ success: true, data: listaCervejas})
+    } catch (err) {
+        console.log(err)
+        res.status(500).send ({success: false, error: 'Erro ao buscar cervejas!'})
+    }
+}
+
+const atualizarCerveja = async (req, res) => {
+    try {
+        const id = req.params.id
+        const { nome, abv, tipo, nacionalidade } = req.body
+        if (!id || !nome || !abv || !tipo || !nacionalidade) {
+            res.status(400).send({message: 'Dados incompletos!!'})
+        }
+        const cervejaAtualizada = { nome, abv, tipo, nacionalidade }
+        const resultado = await Cerveja.update(cervejaAtualizada, { where: { id: id} })
+        res.status(200).send({ message: 'Cerveja atualizada', resultado: resultado })
+    } catch (err) {
+        res.status(500).send({ message: 'Erro na atualizacao' })
     }
 }
 
@@ -21,4 +46,6 @@ const cadastrarCerveja = async (req, res) => {
 
 
 
-export { cadastrarCerveja }
+
+
+export { listarCervejas, cadastrarCerveja, atualizarCerveja }
--- Node_Apuntes_Ruan/AVALIAÇÃO_FINAL_NODE/ORM_Sequelize_Cervejaria/routes/cervejasRoutes.js
+++ Node_Apuntes_Ruan/AVALIAÇÃO_FINAL_NODE/ORM_Sequelize_Cervejaria/routes/cervejasRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 
-import { cadastrarCerveja  } from '../controllers/produtos.js'
+import { listarCervejas, cadastrarCerveja, atualizarCerveja  } from '../controllers/produtos.js'
 
 const router = express.Router()
 
 router.get('/cervejas', listarCervejas)
 router.get('/cervejas/:id', listarCervejasPeloId)
-router.post('/cervejas', cadastrarCeveja)
-router.put('/cervejas/:id', atualizarCeveja)
+router.post('/cervejas', cadastrarCerveja)
+router.put('/cervejas/:id', atualizarCerveja)
 router.delete('/cervejas/:id', apagarCerveja)
 
 
